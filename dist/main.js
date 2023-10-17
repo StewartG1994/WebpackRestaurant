2107eaa21ba4494a29a7aec1501e52e4326d5a7d
Stewart Gliniecki <stewgliniecki@gmail.com>
2023-10-17 17:37:49 +0100

    webpack setup inc live server

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+consw
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLEsiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJwYWNrcmVzdGF1cmFudC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
